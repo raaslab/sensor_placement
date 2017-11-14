013a21c27d43dd08c0e717467d5898708663f96a
Yoonchang Sung <yooncs8@vt.edu>
2017-11-14 18:46:05 -0500

    No bugs for reading data.txt

--- ros_ws/src/spatiotemporal_estimation/scripts/temp_data_read.py
+++ ros_ws/src/spatiotemporal_estimation/scripts/temp_data_read.py
@@ -5,8 +5,9 @@ from std_msgs.msg import String
 
 def reader():
 	fileDir = os.path.dirname(os.path.realpath('__file__'))
-	file = open(os.path.join(fileDir, '../data/data.txt'), ”r”)
-	line = file.readline(1)
+	filePath = os.path.join(fileDir, '../data/data.txt')
+	file = open(filePath)
+	line = file.readline()
 	print "Read Line: %s" % (line)
 	file.close()
 
