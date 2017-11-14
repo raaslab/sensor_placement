61abdb963a233dc13f58f32835eff46e31d40e33
Yoonchang Sung <yooncs8@vt.edu>
2017-11-14 17:46:22 -0500

    Basic format in temp_data_read.py

--- spatiotemporal_estimation/scripts/temp_data_read.py
+++ spatiotemporal_estimation/scripts/temp_data_read.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
 
+def reader():
+
+if __name__ == '__main__':
+    try:
+        reader()
+    except rospy.ROSInterruptException:
+        pass
