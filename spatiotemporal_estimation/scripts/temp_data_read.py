5da480f56652092d6c17abd85de90c6e2db56271
Yoonchang Sung <yooncs8@vt.edu>
2017-11-14 18:00:33 -0500

    Add file reading in temp_data_read.py

--- spatiotemporal_estimation/scripts/temp_data_read.py
+++ spatiotemporal_estimation/scripts/temp_data_read.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 import rospy
+import os
 from std_msgs.msg import String
 
 def reader():
+	fileDir = os.path.dirname(os.path.realpath('__file__'))
+	file = open(os.path.join(fileDir, '../data/data.txt'), ”r”)
+	line = file.readline(1)
+	print "Read Line: %s" % (line)
+	file.close()
 
 if __name__ == '__main__':
     try:
