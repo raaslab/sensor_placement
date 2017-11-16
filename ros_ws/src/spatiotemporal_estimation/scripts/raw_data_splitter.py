b01984b76ff52b93014c8f9372cf4b6f581dcabc
Yoonchang Sung <yooncs8@vt.edu>
2017-11-16 16:46:57 -0500

    Initial setup of reading lines in raw_data_spliter.py

--- ros_ws/src/spatiotemporal_estimation/scripts/raw_data_splitter.py
+++ ros_ws/src/spatiotemporal_estimation/scripts/raw_data_splitter.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
+
+#Written by Yoonchang Sung (yooncs8@vt.edu)
+#This splits the raw dataset into multiple text files corresponding to each sensor.
+#This filters out outliers as well.
+
 import rospy
 import os
 from std_msgs.msg import String
 
-
+#From the dataset, authors mentioned that they used 54 sensors in the environment.
+num_sensor = 54
+sensor_id = 1
 
 def splitter():
 	fileDir = os.path.dirname(os.path.realpath('__file__'))
 	filePath = os.path.join(fileDir, '../data/data.txt')
-	file = open(filePath)
-	line = file.readline().split(" ")
-	print "Read Line: %s" % (line[0])
+	with open(filePath) as file:
+		for line in file:
+			word = line.split(" ")
+			print "Read word: %s" % (word[3])
+			if word[3] == sensor_id:
+				#Filter out outliers.
+			else:
+				sensor_id = sensor_id + 1
+
+	#file = open(filePath)
+	#while 
+	#line = file.readline().split(" ")
+	#print "Read Line: %s" % (line[3])
+
 	file.close()
 
 if __name__ == '__main__':
