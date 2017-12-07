e93ba22f39f1565e8ee41d09ccff94656eb8f669
Yoonchang Sung <yooncs8@vt.edu>
2017-12-06 22:21:59 -0500

    Complete read_comp.m

--- robot_measure_rmse_sim/ST_Dis_Sim_Init.m
+++ robot_measure_rmse_sim/ST_Dis_Sim_Init.m
@@ -1,8 +1,8 @@
 % Spatio-temporal distribution simulation initialization
 
-comp_mean = [];
-comp_cov = [];
-comp_mean_var = [];
-comp_cov_var = [];
+comp.mean = [];
+comp.cov = [];
+comp.mean_var = [];
+comp.cov_var = [];
 
-[num_comp, comp_mean, comp_cov, comp_mean_var, comp_cov_var] = read_comp;
+[num_comp, comp] = read_comp;
