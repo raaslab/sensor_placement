4a5a46aa98c5a510aeef4a5f7d4fc966d98cac56
Yoonchang Sung <yooncs8@vt.edu>
2017-12-10 19:09:35 -0500

    Add tuned hyper params

--- robot_measure_rmse_sim/empirical_rms.m
+++ robot_measure_rmse_sim/empirical_rms.m
@@ -12,7 +12,7 @@ likfunc = @likGauss;              % Gaussian likelihood
 observe = 4*rand(100, 2) - 2; 
 observe_ = offset + scaled*observe(:, 1).*exp(-observe(:, 1).^2 - observe(:, 2).^2);
 
-scatter(observe(:, 1), observe(:, 2));
+% scatter(observe(:, 1), observe(:, 2));
 
 % [X,Y] = meshgrid(x);
 % values = offset + scaled*x.*exp(-x.^2-(y').^2);
@@ -23,7 +23,7 @@ scatter(observe(:, 1), observe(:, 2));
 fixed_point = [0, 0];
 
 % Points for RMSE calculation
-x = -2:0.1:2;
+x = -2:0.05:2;
 y = x;
 [X, Y] = meshgrid(x);
 xs = [X(:), Y(:)];
@@ -32,20 +32,30 @@ actual = offset + scaled*xs(:, 1).*exp(-xs(:, 1).^2 - xs(:, 2).^2);
 error_prior = [];
 error_post = [];
 
-for subset_size = 1:100
+%Tuned hyper params by using 100 sampling points + [0,0]
+hyp2.cov = [-0.0349, 1.9444];
+hyp2.lik = -45.8462;
+
+for subset_size = 1:size(observe_,1)
 	hyp = struct('mean', [], 'cov', [0 0], 'lik', -1);
-	%hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, observe(1:subset_size, :), observe_(1:subset_size, :))
-	[mu s2] = gp(hyp, @infGaussLik, meanfunc, covfunc, likfunc, observe(1:subset_size, :), observe_(1:subset_size, :), xs);
+% 	hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, observe(1:subset_size, :), observe_(1:subset_size, :))
+	[mu s2] = gp(hyp2, @infGaussLik, meanfunc, covfunc, likfunc, observe(1:subset_size, :), observe_(1:subset_size, :), xs);
 	error_prior = [error_prior; sum(sum(abs(mu - actual).^2))];
 end
 
-for subset_size = 1:100
+for subset_size = 1:size(observe_,1)
 	hyp = struct('mean', [], 'cov', [0 0], 'lik', -1);
-	%hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, [observe(1:subset_size, :); 0, 0], [observe_(1:subset_size, :); offset])
-	[mu s2] = gp(hyp, @infGaussLik, meanfunc, covfunc, likfunc, [observe(1:subset_size, :);0, 0], [observe_(1:subset_size, :); offset], xs);
+% 	hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, [observe(1:subset_size, :); 0, 0], [observe_(1:subset_size, :); offset])
+	[mu s2] = gp(hyp2, @infGaussLik, meanfunc, covfunc, likfunc, [observe(1:subset_size, :);0, 0], [observe_(1:subset_size, :); offset], xs);
 	error_post = [error_post; sum(sum(abs(mu - actual).^2))];
 end
 
+%Submodularity
+figure(1)
+plot(1:size(observe_,1),error_post-error_prior,'r');
+
+figure(2)
+plot(error_post)
 
 
 % mu = reshape(mu, size(X));
