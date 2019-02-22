3637e8f241505515944ad77da41e4206454512f7
Varun Suryan <varunsuryan9@gmail.com>
2019-02-22 16:37:20 -0500

    farm boundary redefined

--- ground_truth.m
+++ ground_truth.m
@@ -17,14 +17,9 @@ environment = [135, 230, 230, 190, 190, 140, 80, 150; 20, 20, 60, 70, 115, 115, 
 in = double(inpolygon(Xq, Yq, environment(1, :), environment(2, :)));
 ex_polygon = in == 0;
 
-% Xq(ex_polygon) = nan; Yq(ex_polygon) = nan; 
-Zq(ex_polygon) = nan;
+Xq(ex_polygon) = nan; Yq(ex_polygon) = nan; Zq(ex_polygon) = nan;
 
-
-% size(farm_rectangle)
-% size(in_rectangle)
-
-figure();
-h = surf(Xq, Yq, Zq + 0*randn(size(Zq)));
-set(h,'LineStyle','none')
-axis equal; grid off;
+% figure();
+% h = surf(Xq, Yq, Zq);
+% set(h,'LineStyle','none')
+% axis equal; grid off;
