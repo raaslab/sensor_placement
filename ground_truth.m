15573e97affcb32b25741366745fb2f6b8fbe580
Varun Suryan <varunsuryan9@gmail.com>
2019-03-09 22:40:11 -0500

    mis added and caculated

--- ground_truth.m
+++ ground_truth.m
@@ -5,7 +5,14 @@ close all;
 load('OM_dataset.mat');
 Xq = flipud(Xq);
 Yq = flipud(Yq);
-Zq = flipud(Zq);
+Zq = flipud(Zq)-10;
+
+
+meanfunc = @meanConst;                    % empty: don't use a mean function
+covfunc = @covSEiso;              % Squared Exponental covariance function
+likfunc = @likGauss;              % Gaussian likelihood
+
+hyp = struct('mean', [3.9066], 'cov', [1.1341 -1.3895], 'lik', -5.0313);
 
 environment = [135, 230, 230, 190, 190, 140, 80, 150; 20, 20, 60, 70, 115, 115, 50, 50];
 
@@ -14,12 +21,59 @@ environment = [135, 230, 230, 190, 190, 140, 80, 150; 20, 20, 60, 70, 115, 115, 
 % hold on;
 % axis equal;
 
+%% define the farm boundary
 in = double(inpolygon(Xq, Yq, environment(1, :), environment(2, :)));
 ex_polygon = in == 0;
-
 Xq(ex_polygon) = nan; Yq(ex_polygon) = nan; Zq(ex_polygon) = nan;
 
-% figure();
-% h = surf(Xq, Yq, Zq);
-% set(h,'LineStyle','none')
-% axis equal; grid off;
+inputDesign = [Xq(:), Yq(:), Zq(:)];
+
+inputDesign(any(isnan(inputDesign), 2), :) = [];
+% Xq = Xq(min(environment(1, :)):max(environment(1, :)), min(environment(2, :)):max(environment(2, :)));
+% Yq = Yq(min(environment(1, :)):max(environment(1, :)), min(environment(2, :)):max(environment(2, :)));
+% Zq = Zq(min(environment(1, :)):max(environment(1, :)), min(environment(2, :)):max(environment(2, :)));
+
+% hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, inputDesign(1:10:end, 1:2), inputDesign(1:10:end, end));
+% define lipschitz constant (ppm/meter)
+lipschitz = 3;
+l = exp(1.1341); sigma_0 = exp(-1.3895); omega = exp(-5.0313);
+N = 100;
+epsilon = .05; delta = 0.5795;
+arg1 = (sigma_0^2+omega^2)/(N*sigma_0^2);
+arg2 = 1-epsilon/(sqrt(2)*sigma_0^2*erfinv(delta));
+% r_max = l*sqrt(-log(arg1*arg2));
+r_max = 20;
+
+
+% Covering disks
+[diskX, diskY] = meshgrid(min(environment(1, :)):r_max:max(environment(1, :)), min(environment(2, :)):r_max:max(environment(2, :)));
+
+points = [diskX(:), diskY(:)];
+new = size(points, 1);
+i = 1;
+% pause();
+% Calculate MIS and denote centers of MIS disks by 'points'
+while i < new
+	index_c = [];
+	for j = i + 1 : size(points, 1)
+		if (points(i, 1) - points(j, 1))^2 + (points(i, 2) - points(j, 2))^2 <= 4*r_max^2
+			index_c = [index_c; j];
+		end
+	end
+	for len = size(index_c, 1) : -1 : 1
+		points(index_c(len, 1), :) = [];
+	end
+	new = size(points, 1);
+	i = i + 1;
+end
+
+% delete first two disks
+points = points(3:end, :);
+
+figure();
+h = surf(Xq, Yq, Zq);
+hold on;
+% scatter(diskX(:), diskY(:));
+set(h,'LineStyle','none')
+axis equal; grid off;
+viscircles([points(:, 1), points(:, 2)], r_max*ones(size(points, 1), 1));
--- test_gp.m
+++ test_gp.m
@@ -1,28 +1,47 @@
-x = linspace(-4, 4, 100)';               
-y = sin(3*x) + normrnd(x, 0.2);  
-xs = linspace(-4, 4, 61)';         
+% x = linspace(-4, 4, 100)';               
+% y = sin(3*x) + normrnd(x, 0.2);  
+clc;
+clear all;
+close all;
+epsilon = .075; delta = 0.9;
+r_max_var = 15:0.5:30;
+val = [];
+for r_max = r_max_var
+	val = [val, myfun(r_max)];
+end
 
-% x = gpml_randn(0.8, 20, 1)                 % 20 training inputs
-%   y = sin(3*x) + 0.1*gpml_randn(0.9, 20, 1);  % 20 noisy training targets
-%   xs = linspace(-3, 3, 61)';                  % 61 test inputs 
+plot(r_max_var, val)
+hold on;
+line([r_max_var(1), r_max_var(end)], [epsilon/(sqrt(2)*erfinv(delta)), epsilon/(sqrt(2)*erfinv(delta))]);
 
-length_scale = 10;
-signal_std = 10;
-noise_std = 0.2; 
+function expression = myfun(r_max)
+	xs = [0, 0];
+	theta = linspace(-pi, pi/2, 500)';
+	% theta = zeros(50, 1);
+	x = r_max*cos(theta); y = r_max*sin(theta); z = x.^2 + y.^2;	
 
-meanfunc = [];                    % empty: don't use a mean function
-covfunc = @covSEiso;              % Squared Exponental covariance function
-likfunc = @likGauss;              % Gaussian likelihood
+	% x = gpml_randn(0.8, 20, 1)                 % 20 training inputs
+	%   y = sin(3*x) + 0.1*gpml_randn(0.9, 20, 1);  % 20 noisy training targets
+	%   xs = linspace(-3, 3, 61)';                  % 61 test inputs 	
 
+	length_scale = 10;
+	signal_std = 10;
+	noise_std = 0.2; 	
 
-hyp = struct('mean', [], 'cov', [log(length_scale) log(signal_std)], 'lik', log(noise_std));
+	meanfunc = [];                    % empty: don't use a mean function
+	covfunc = @covSEiso;              % Squared Exponental covariance function
+	likfunc = @likGauss;              % Gaussian likelihood	
+	
 
-% hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, x, y);
+	hyp = struct('mean', [], 'cov', [log(length_scale) log(signal_std)], 'lik', log(noise_std));	
 
-[mu s2] = gp(hyp, @infGaussLik, meanfunc, covfunc, likfunc, x, y, xs);
+	% hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, x, y);	
 
+	[mu s2] = gp(hyp, @infGaussLik, meanfunc, covfunc, likfunc, [x, y], z, xs);	
 
-f = [mu + 2*sqrt(s2); flipdim(mu-2*sqrt(s2),1)];
-fill([xs; flipdim(xs,1)], f, [7 7 7]/8)
-hold on; plot(xs, mu); plot(x, y, '+')
-
+	expression = signal_std^2*(1-exp(-r_max^2/length_scale^2)*numel(theta)*signal_std^2/...
+		(signal_std^2+noise_std^2));	
+	% f = [mu + 2*sqrt(s2); flipdim(mu-2*sqrt(s2),1)];
+	% fill([xs; flipdim(xs,1)], f, [7 7 7]/8)
+	% hold on; plot(xs, mu); plot(x, y, '+')
+end
